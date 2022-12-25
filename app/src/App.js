d82547618a59cfb5fdd160ca8c07222101069bdd
pswebcom <pswebco@gmail.com>
2022-12-25 14:56:08 -0500

    Header should see in every page

--- app/src/App.js
+++ app/src/App.js
@@ -5,11 +5,13 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
