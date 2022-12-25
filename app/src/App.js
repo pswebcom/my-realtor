5c6e69f993731c5e37f16fb7fc6b4b4b52e98358
pswebcom <pswebco@gmail.com>
2022-12-25 14:38:15 -0500

    basic routing done

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,26 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1 class="text-3xl font-bold underline text-clifford">Hello world!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
