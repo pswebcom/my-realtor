a50229f3f18b6257a5ff0ed311d398416355ab8c
pswebcom <pswebco@gmail.com>
2022-12-26 00:15:14 -0500

    signIn functionality... used useState hook & prevState to hold input data

--- app/src/pages/SignIn.jsx
+++ app/src/pages/SignIn.jsx
@@ -1,6 +1,21 @@
 import React, { useState } from "react";
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+  console.log(formData);
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
@@ -13,9 +28,29 @@ const SignIn = () => {
           />
         </div>
 
+        {/*1-- use tailwind css forms-----tailwindlabs/tailwindcss-forms
+         */}
+
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form>
-            <input type="email" id="email" className="w-full" />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+            />
+            <div>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={onChange}
+                placeholder="Password"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mt-2"
+              />
+            </div>
           </form>
         </div>
       </div>
