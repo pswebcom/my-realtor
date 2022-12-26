37403f92c78256877b31ecaa496808fdfdf12913
pswebcom <pswebco@gmail.com>
2022-12-26 00:30:48 -0500

    signIn functionality...hided password will show on eye button click

--- app/src/pages/SignIn.jsx
+++ app/src/pages/SignIn.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 
 const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -28,9 +30,6 @@ const SignIn = () => {
           />
         </div>
 
-        {/*1-- use tailwind css forms-----tailwindlabs/tailwindcss-forms
-         */}
-
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form>
             <input
@@ -41,15 +40,27 @@ const SignIn = () => {
               placeholder="Email address"
               className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
             />
-            <div>
+            <div className="relative">
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 id="password"
                 value={password}
                 onChange={onChange}
                 placeholder="Password"
                 className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mt-2"
               />
+              {/*-- npm i react-icons  */}
+              {showPassword ? (
+                <AiFillEyeInvisible
+                  className="absolute right-3 top-5 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              ) : (
+                <AiFillEye
+                  className="absolute right-3 top-5 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              )}
             </div>
           </form>
         </div>
