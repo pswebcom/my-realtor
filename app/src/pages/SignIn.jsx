4e280830344c243eac26f4826c52d2c39970d8c2
pswebcom <pswebco@gmail.com>
2022-12-26 15:02:52 -0500

    signIn functionality...register and forgot pwd links completed

--- app/src/pages/SignIn.jsx
+++ app/src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -38,16 +39,16 @@ const SignIn = () => {
               value={email}
               onChange={onChange}
               placeholder="Email address"
-              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6 border"
             />
-            <div className="relative">
+            <div className="relative mb-6">
               <input
                 type={showPassword ? "text" : "password"}
                 id="password"
                 value={password}
                 onChange={onChange}
                 placeholder="Password"
-                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mt-2"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out border"
               />
               {/*-- npm i react-icons  */}
               {showPassword ? (
@@ -61,6 +62,25 @@ const SignIn = () => {
                   onClick={() => setShowPassword((prevState) => !prevState)}
                 />
               )}
+            </div>
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="mb-6">
+                Dont't have an account ?{" "}
+                <Link
+                  to="/sign-up"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                >
+                  Register
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/forgot-password"
+                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
+                >
+                  Forgot Password
+                </Link>
+              </p>
             </div>
           </form>
         </div>
